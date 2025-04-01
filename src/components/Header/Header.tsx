2783d639b35895c5bf09d8078767b7b745c53fa9
Yosber Guerrero <yosbergc@gmail.com>
2025-04-01 00:14:59 -0400

    Added search component for desktop

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,15 @@
 import styles from './Header.module.css'
 import { Login } from '../Login/Login'
+import { Search } from '../Search/Search'
 import Image from 'next/image'
 function Header() {
     return (
     <header className={styles.appHeader}>
         <Image src="/logo.png" alt="Logo" width={30} height={30}/>
-        <Login />
+        <section className={styles.secondPart}>
+            <Search />
+            <Login />
+        </section>
     </header>
     )
 }
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,6 +1,22 @@
+'use client'
+import { useWindoWidth } from "@/hooks/useWindoWidth"
+import { BiSearch } from "react-icons/bi"
+import styles from './Search.module.css'
 function Search() {
+    const [width] = useWindoWidth()
     return (
-        <input placeholder="Twenty One Pilots..."></input>
+        <section className={styles.searchContainer}>
+            {
+                width < 768 && (
+                    <BiSearch color="white" size={25}/>
+            )
+            }
+            {
+                width >= 768 && (
+                    <input type="text" placeholder="Twenty One Pilots... " className={styles.searchInput}/>
+                )
+            }
+        </section>
     )
 }
 export { Search }
